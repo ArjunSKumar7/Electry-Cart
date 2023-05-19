43e0f4c56a65a1109bcd4cf6800926efc5127140
ArjunSKumar7 <arjunskumar2000@gmail.com>
2023-05-19 12:25:53 +0530

    product offer remove func1

--- controllers/admincontroller/adminOfferController.js
+++ controllers/admincontroller/adminOfferController.js
@@ -1,44 +1,31 @@
-const adminOfferHelper =require("../../helpers/adminHelpers/adminOfferHelper")
-const adminProductHelpers =require("../../helpers/adminHelpers/adminProductHelpers")
+const adminOfferHelper = require("../../helpers/adminHelpers/adminOfferHelper");
+const adminProductHelpers = require("../../helpers/adminHelpers/adminProductHelpers");
 
+module.exports = {
+  productoffer: async (req, res) => {
+    adminProductHelpers.getViewProduct().then((response) => {
+      res.render("admin/add-productoffer", {
+        layout: "adminLayout",
+        response,
 
-module.exports={
+        adminlogin: true,
+      });
+    });
+  },
 
+  productofferajax: async (req, res) => {
+    adminProductHelpers.productofferajax(req.body).then((response) => {
+      res.json({ status: true });
+    });
+  },
 
-    productoffer:async(req,res)=>{
-        adminProductHelpers.getViewProduct().then((response) => {
-            res.render("admin/add-productoffer", {
-                layout: "adminLayout",
-                response,
-        
-                adminlogin: true,
-              });
-
-
-        })
-    },
-
-
-    productofferajax:async(req,res)=>{
-        adminProductHelpers.productofferajax(req.body).then((response)=>{
-res.json({status:true})
-        })
-    },
-
-
-
-removeofferajax: async (req, res) => {
+  removeofferajax: async (req, res) => {
     try {
-    
-  
       adminOfferHelper.removeofferajax(req.body).then((response) => {
         res.json(response);
       });
     } catch (err) {
       console.log("Error occurred while removing the offer:", err);
     }
-  }
-  
-    
-
-}
+  },
+};
